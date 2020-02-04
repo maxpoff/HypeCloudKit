728ecce19cec7a32dc01ac2ad84e8a971fdba301
Maxwell Poffenbarger <maxwellpoffenbarger@Maxwells-MacBook-Pro.local>
2020-02-04 11:23:03 -0700

    created Hype model

---
+++ HypeCloudKit/HypeCloudKit/Models/Hype.swift
@@ -0,0 +1,49 @@
+//
+//  Hype.swift
+//  HypeCloudKit
+//
+//  Created by Maxwell Poffenbarger on 2/4/20.
+//  Copyright © 2020 Maxwell Poffenbarger. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+struct HypeStrings {
+    fileprivate static let bodyKey = "body"
+    fileprivate static let timestampKey = "timestamp"
+    fileprivate static let recordTypeKey = "Hype"
+}//End of struct
+
+class Hype {
+    
+    var body: String
+    var timestamp: Date
+    
+    init(body: String, timestamp: Date = Date()) {
+        self.body = body
+        self.timestamp = timestamp
+    }
+}//End of class
+
+extension CKRecord {
+    
+    convenience init(hype: Hype) {
+        self.init(recordType: HypeStrings.recordTypeKey)
+        
+        self.setValuesForKeys([
+            HypeStrings.bodyKey : hype.body,
+            HypeStrings.timestampKey : hype.timestamp
+        ])
+    }
+}//End of extension
+
+extension Hype {
+    
+    convenience init?(ckRecord: CKRecord) {
+        guard let body = ckRecord[HypeStrings.bodyKey] as? String,
+            let timestamp = ckRecord[HypeStrings.timestampKey] as? Date
+            else {return nil}
+        self.init(body: body, timestamp: timestamp)
+    }
+}//End of extension
